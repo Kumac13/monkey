129f12d48284a4a38f2c4f48620baab479cd1816
kumac13 <foohilack@gmail.com>
2023-04-12 09:46:35 +0900

    feat: add support for Display

--- src/token.rs
+++ src/token.rs
@@ -1,4 +1,7 @@
-#[derive(Debug, PartialEq, Clone)]
+use std::collections::HashMap;
+use std::fmt::{self, Display};
+
+#[derive(Debug, PartialEq, Clone, Hash)]
 pub enum TokenKind {
     ILLEGAL,
     EOF,
@@ -35,8 +38,53 @@ pub struct Token {
     pub literal: String,
 }
 
+impl Display for TokenKind {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let s = match self {
+            TokenKind::ILLEGAL => "ILLEGAL",
+            TokenKind::EOF => "EOF",
+            TokenKind::INTEGER => "INTEGER",
+            TokenKind::IDENTIFIER => "IDENTIFIER",
+            TokenKind::ASSIGN => "=",
+            TokenKind::PLUS => "+",
+            TokenKind::MINUS => "-",
+            TokenKind::BANG => "!",
+            TokenKind::ASTERISK => "*",
+            TokenKind::SLASH => "/",
+            TokenKind::LT => "<",
+            TokenKind::GT => ">",
+            TokenKind::EQ => "==",
+            TokenKind::NOT_EQ => "!=",
+            TokenKind::COMMA => ",",
+            TokenKind::SEMICOLON => ";",
+            TokenKind::LPAREN => "(",
+            TokenKind::RPAREN => ")",
+            TokenKind::LBRACE => "{",
+            TokenKind::RBRACE => "}",
+            TokenKind::FUNCTION => "FUNCTION",
+            TokenKind::LET => "LET",
+            TokenKind::TRUE => "TRUE",
+            TokenKind::FALSE => "FALSE",
+            TokenKind::IF => "IF",
+            TokenKind::ELSE => "ELSE",
+            TokenKind::RETURN => "RETURN",
+        };
+        write!(f, "{}", s)
+    }
+}
+
 impl Token {
     pub fn new(kind: TokenKind, literal: String) -> Token {
         Token { kind, literal }
+    }
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Token{{ kind: {}, literal: {} }}",
+            self.kind, self.literal
+        )
     }
 }
